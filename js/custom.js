c052e5c819f35a8d91ff90d1e02e5f72935c2e46
DESKTOP-G0QF9A3\asutp Aplisens <anderzik91@gmail.com>
2024-07-06 13:53:38 +0300

    add style shop

--- js/custom.js
+++ js/custom.js
@@ -6,3 +6,12 @@
   
 
 */
+
+document.addEventListener('DOMContentLoaded', () => {
+    const selectElement = document.getElementById('customSelect');
+    selectElement.style.fontFamily = '"Jura", sans-serif';
+
+    for (let i = 0; i < selectElement.options.length; i++) {
+        selectElement.options[i].style.fontFamily = '"Jura", sans-serif';
+    }
+});
